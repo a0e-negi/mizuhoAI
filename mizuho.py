5d6ce71f87629dec805cb8911a00c95406e74aed
ruesei-koseki <51506011+ruesei-koseki@users.noreply.github.com>
2022-07-17 15:42:15 +0900

    Add files via upload

--- mizuho.py
+++ mizuho.py
@@ -100,7 +100,7 @@ def looking(x, reply=True):
                                     if reply:
                                         if not bool(re.search(settings["mynames"], data["sentence"][i+1+iii][0])) and i != len(data["sentence"]) and lastSentence != data["sentence"][i+1+iii][0] and lastSentenceInput != data["sentence"][i+1+iii][0]:
                                             flag = True
-                                            for iiiii in range(5):
+                                            for iiiii in range(100):
                                                 if i+1+iii+iiiii < len(data["sentence"]) - 2:
                                                     if data["sentence"][i+1+iii+iiiii][0] == "×":
                                                         flag = False
@@ -174,7 +174,7 @@ def looking(x, reply=True):
                                     if reply:
                                         if not bool(re.search(settings["mynames"], data["sentence"][i+1+iii][0])) and i != len(data["sentence"]) and lastSentence != data["sentence"][i+1+iii][0] and lastSentenceInput != data["sentence"][i+1+iii][0]:
                                             flag = True
-                                            for iiiii in range(5):
+                                            for iiiii in range(100):
                                                 if i+1+iii+iiiii < len(data["sentence"]) - 2:
                                                     if data["sentence"][i+1+iii+iiiii][0] == "×":
                                                         flag = False
@@ -241,7 +241,7 @@ def looking(x, reply=True):
                                     if reply:
                                         if not bool(re.search(settings["mynames"], data["sentence"][i+1+iii][0])) and i != len(data["sentence"]) and lastSentence != data["sentence"][i+1+iii][0] and lastSentenceInput != data["sentence"][i+1+iii][0]:
                                             flag = True
-                                            for iiiii in range(5):
+                                            for iiiii in range(100):
                                                 if i+1+iii+iiiii < len(data["sentence"]) - 2:
                                                     if data["sentence"][i+1+iii+iiiii][0] == "×":
                                                         flag = False
@@ -310,7 +310,7 @@ def looking(x, reply=True):
                                     if reply:
                                         if not bool(re.search(settings["mynames"], data["sentence"][i+1+iii][0])) and i != len(data["sentence"]) and lastSentence != data["sentence"][i+1+iii][0] and lastSentenceInput != data["sentence"][i+1+iii][0]:
                                             flag = True
-                                            for iiiii in range(5):
+                                            for iiiii in range(100):
                                                 if i+1+iii+iiiii < len(data["sentence"]) - 2:
                                                     if data["sentence"][i+1+iii+iiiii][0] == "×":
                                                         flag = False
@@ -365,7 +365,7 @@ def tsuzuki(add=True):
             if data["sentence"][heart][1] == heartLastSpeaker and heart != len(data["sentence"]) - 1 and not bool(re.search(settings["mynames"], data["sentence"][heart][0])) and lastSentence != data["sentence"][heart][0] and lastSentenceInput != data["sentence"][heart][0]:
 
                 flag = True
-                for iiiii in range(5):
+                for iiiii in range(100):
                     if heart+iiiii < len(data["sentence"]) - 2:
                         if data["sentence"][heart+iiiii][0] == "×":
                             flag = False
